e50980cb3b33c10b430eafda392fed1baf15890d
Catchxu <kaichenxu358@gmail.com>
2026-01-22 02:34:54 +0800

    Update pretrain_sfm

--- src/sccafm/builder.py
+++ src/sccafm/builder.py
@@ -26,29 +26,34 @@ LOSS_REGISTRY = {
 
 
 # ----------------- Build Functions -----------------
-def build_model(cfg: dict):
+def build_model(cfg: dict, **runtime_kwargs):
     name = cfg["name"]
     kwargs = cfg.get("kwargs", {})
 
     if name not in MODEL_REGISTRY:
         raise ValueError(f"Unknown model '{name}'")
-
+    
+    kwargs.update(runtime_kwargs)
     return MODEL_REGISTRY[name](**kwargs)
 
-def build_tokenizer(cfg: dict):
+
+def build_tokenizer(cfg: dict, **runtime_kwargs):
     name = cfg["name"]
     kwargs = cfg.get("kwargs", {})
 
     if name not in TOKENIZER_REGISTRY:
         raise ValueError(f"Unknown tokenizer '{name}'")
 
+    kwargs.update(runtime_kwargs)
     return TOKENIZER_REGISTRY[name](**kwargs)
 
-def build_loss(cfg: dict):
+
+def build_loss(cfg: dict, **runtime_kwargs):
     name = cfg["name"]
     kwargs = cfg.get("kwargs", {})
 
     if name not in LOSS_REGISTRY:
         raise ValueError(f"Unknown loss '{name}'")
 
+    kwargs.update(runtime_kwargs)
     return LOSS_REGISTRY[name](**kwargs)
--- src/sccafm/runner/pretrain_sfm.py
+++ src/sccafm/runner/pretrain_sfm.py
@@ -1,11 +1,13 @@
 import argparse
 
-from sccafm.load import load_resources, load_cfg
+from sccafm.load import load_cfg
 from sccafm.builder import build_model, build_loss, build_tokenizer
+from sccafm.trainer import sfm_trainer
+
 
 def main():
     parser = argparse.ArgumentParser(description="Pretrain scCAFM-SFM model")
-    parser.add_argument("--config", type=str, required=True, help="Meta config YAML path")
+    parser.add_argument("--config", type=str, default="meta.yaml", help="Meta config YAML path")
     parser.add_argument("--override", nargs="*", default=[], help="Optional overrides: key=value")
     args = parser.parse_args()
 
@@ -18,12 +20,13 @@ def main():
 
     data_cfg = pretrain_cfg["datasets"]
     loss_cfg = pretrain_cfg["loss"]
+    train_cfg = pretrain_cfg["train"]
 
     for item in args.override:
         key, value = item.split("=")
         keys = key.split(".")
     
-        d = pretrain_cfg
+        d = train_cfg
         for k in keys[:-1]:
             d = d[k]
 
@@ -36,6 +39,23 @@ def main():
             value = float(value)
         d[keys[-1]] = value
     
-    model = build_model(model_cfg)
-    tokenizer = build_tokenizer(tokenizer_cfg)
+    token_dict = data_cfg["token_dict"]
+    adata_files = data_cfg["adata_files"]
+    
+    model = build_model(model_cfg, token_dict=token_dict)
+    tokenizer = build_tokenizer(tokenizer_cfg, token_dict=token_dict)
     loss = build_loss(loss_cfg)
+
+    sfm_trainer(
+        model=model,
+        adata_files=adata_files,
+        tokenizer=tokenizer,
+        criterion=loss,
+        **train_cfg
+    )
+
+
+
+
+if __name__ == "__main__":
+    main()
