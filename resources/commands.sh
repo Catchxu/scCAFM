e60a35474331ee511f46c6ae8976b123568e473e
Catchxu <kaichenxu358@gmail.com>
2026-02-27 03:06:23 +0800

    Update command cookbook

--- resources/commands.sh
+++ resources/commands.sh
@@ -10,11 +10,11 @@
 
 # model training
 python3 scripts/run_pretrain.py --nproc-per-node 4
-nohup python3 scripts/run_pretrain.py --nproc-per-node 4 > /dev/null 2>&1 &
+setsid nohup python3 scripts/run_pretrain.py --nproc-per-node 4 > /dev/null 2>&1 < /dev/null &
 
 # evaluation
 python3 scripts/run_eval_grn.py --nproc-per-node 4
-nohup python3 scripts/run_eval_grn.py --nproc-per-node 4 > /dev/null 2>&1 &
+setsid nohup python3 scripts/run_eval_grn.py --nproc-per-node 4 > /dev/null 2>&1 < /dev/null &
 
 # pipeline
-nohup bash scripts/run_grn.sh > /dev/null 2>&1 &
+setsid nohup bash scripts/run_grn.sh > /dev/null 2>&1 < /dev/null &
